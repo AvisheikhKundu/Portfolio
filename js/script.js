85871eba6a60e3753be3f0c21e9b8f98fe1298b9
AvisheikhKundu <avisheikh76@gmail.com>
2023-02-16 21:36:02 +0600

    all file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Programmer ","Web Developer","UI/UX designer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
